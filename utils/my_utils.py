1f32cdfbcf64b4fa3be21997ccd791598abe1e7d
leschnitzel <epicxschnitzel@yahoo.de>
2023-01-23 11:07:00 +0100

    comments

--- utils/my_utils.py
+++ utils/my_utils.py
@@ -8,7 +8,7 @@ import scipy.stats as stats
 
 ######################################################################################################
 #
-#                                         PART #2: UTILITY
+#                                         UTILITY FUNCTIONS
 #
 ######################################################################################################
 
@@ -37,13 +37,10 @@ def codons(seq, der_frame):
 
     # add positions of start and stop codons to lists
     while (seq and counter < 3):
-
         for i in range(start, len(seq), 3):
             codon = seq[i:i + 3]
-
             if (codon == 'ATG'):
                 lst1[start].append(i)
-
             if (codon in stops):
                 lst2[start].append(i)
 
@@ -53,6 +50,7 @@ def codons(seq, der_frame):
 
     # for each frame extract ORFs
     frame = der_frame
+
     if len(lst1[frame]) > 0 and len(lst2[frame]) > 0:  # at least one start and one stop codon exist
 
         currentStart = 0
@@ -60,11 +58,11 @@ def codons(seq, der_frame):
 
         while currentStart < len(lst1[frame]) and currentStop < len(lst2[frame]):
             if lst1[frame][currentStart] < lst2[frame][currentStop]:  # this is the condition for a valid orf
-
+                
                 orfseq = seq[lst1[frame][currentStart]:lst2[frame][currentStop]]
                 start_pos = lst1[frame][currentStart]
                 stop_pos = lst2[frame][currentStop]
-
+                
                 if len(orfseq) >= minOrfLength:
                     countOrfs += 1
 
@@ -85,106 +83,81 @@ def codons(seq, der_frame):
 
 ######################################################################################################
 
-def orf_column(data,
-               frame,
-               column_name='ORFs'):
-    new_column = []
-
-    if frame < 3:
-        for row in data.index:
-            orfcount = codons(data['utr'][row], frame)
-            new_column.append(orfcount)
-
-        data[str(column_name)] = new_column
-    elif frame >= 3:
-        print('IMPOSSIBLE FRAME!')
-
+def orf_column(data, frame):
+    '''
+    param data: dataframe containing a column with sequences of RNA 5' untranslated regions
+    param frame: the biological reading frame to count uORFs in (0,1 or 2)
+    param column_name: name of the column to save uORF counts in
+    output: dataframe with appended column denoting number of uORFs in each sequence
+    '''
+    assert frame < 3, 'There are only three biological reading frames.'
+    column_name='orfs_outframe_'+str(frame) if frame in [0,1] else 'orfs_inframe'
+    data[column_name]=[codons(row, frame) for row in data['utr']]
     return data
-
 
 ######################################################################################################
 
 def count_augs(data, frame):
-    new = []
+    '''
+    counts frame-wise number of ATG motifs in each utr sequence and adds a saves them in a new column
+    '''
+    #split_seq = [data['utr'][i][frame::3] for i in data.index]
+    #new=[seq.count('ATG') for seq in split_seq]
 
+    new = []
     for row in data.index:
         sequence = data['utr'][row]
+        split_seq = [sequence[i:i + 3] for i in range(frame, len(sequence), 3)]
+        new.append(split_seq.count('ATG'))
 
-        # This is one of the alternative frames (out-of-frame)
-        if frame == 0:
-            split_seq = [sequence[i:i + 3] for i in range(0, len(sequence), 3)]
-
-        # This is the other alternative frame (out-of-frame)
-        elif frame == 1:
-            split_seq = [sequence[i:i + 3] for i in range(1, len(sequence), 3)]
-
-            # For 50-nt UTRs from Sample et al., this means 'in-frame'
-        else:
-            split_seq = [sequence[i:i + 3] for i in range(2, len(sequence), 3)]
-
-        # True, if ATG present, False if not
-        counter = split_seq.count('ATG')
-
-        # Append indicator for each row
-        new.append(counter)
-
-    if frame == 2:
-        data['aug_inframe'] = new
-    elif frame == 1:
-        data['aug_outframe_1'] = new
-    else:
-        data['aug_outframe_0'] = new
+    column_name='aug_outframe_'+str(frame) if frame in [0,1] else 'aug_inframe'
+    data[column_name]=new
 
     return data
 
-
+   
 ######################################################################################################
 
 def extract_overlaps(data):
+    '''
+    This function creates a new column 'overlapping_orfs' in the input dataframe 'data'
+    and populates it with information about whether the ORFs present in the UTR are overlapping or not.
+    '''
     new_column = []
-
     for x in data.index:
-
         # empty positions of all orfs from list after each entry
         uorf_pos_list = []
-
         # aggregate positions of all orfs in utr
         for column in ['orfs_inframe', 'orfs_outframe_0', 'orfs_outframe_1']:
             for orf in range(0, data[column][x][0]):
                 uorf_pos_list.append(range(data[column][x][1][orf],
                                            data[column][x][2][orf]))
-
         # in case the sequence has multiple uorfs
         if len(uorf_pos_list) >= 2:
-
             # aggregate positions of orfs as sets
             for r in range(len(uorf_pos_list)):
                 uorf_pos_list[r] = set(uorf_pos_list[r])
-
             # return intersection of sets
             inter = list(set.intersection(*uorf_pos_list))
-
             # if orfs are non-overlapping, length is 0
             if len(inter) == 0:
                 new_column.append(False)
-
             # if orfs are overlapping, length is longer than 0
             elif len(inter) > 0:
                 new_column.append(True)
-
-
         elif len(uorf_pos_list) <= 1:
-
             new_column.append('none')
-
     data['overlapping_orfs'] = new_column
-
     return data
 
 
 ######################################################################################################
 
 def extract_orf_length(data):
+    '''
+    This function creates a new column 'orf_length' in the input dataframe 'data'
+    and populates it with the length of the ORF(s) present in the UTR.
+    '''
     new_column = []
 
     for x in data.index:
@@ -212,6 +185,10 @@ def extract_orf_length(data):
 ######################################################################################################
 
 def extract_orf_number(data):
+    '''
+    quantifies the total number of uORFs in each utr sequence in the dataframe
+    and saves them in a new column called 'orf_number'
+    '''
     new_column = []
 
     for x in data.index:
@@ -233,35 +210,34 @@ def extract_orf_number(data):
     return data
 ######################################################################################################
 
-def extract_aug_number(dataframe):
-    new_column = []
-
-    for i in dataframe['utr'].index:
-        aug_count = dataframe['utr'][i].count('ATG')
-        new_column.append(aug_count)
-
-    dataframe['aug_number'] = new_column
-
-    return dataframe
+def extract_aug_number(df):
+    '''
+    quantifies the total number of ATG motifs in each utr sequence in the dataframe
+    and saves them in a new column called 'aug_number'
+    '''
+    df['aug_number'] = df['utr'].str.count('ATG')
+    return df
 
 ######################################################################################################
 
 
 def analyze(df):
-    df = orf_column(df, 2, 'orfs_inframe')
-    df = orf_column(df, 0, 'orfs_outframe_0')
-    df = orf_column(df, 1, 'orfs_outframe_1')
+    '''
+    input: dataframe with utr sequences and corresponding measures
+    output: dataframe withh columns for orf measures
 
-    df = count_augs(df, 2)
-    df = count_augs(df, 0)
-    df = count_augs(df, 1)
+    '''
+    #frame-wise number of uORFs/AUGs
+    for frame in [2, 0, 1]:
+        df = orf_column(df, frame)
+        df = count_augs(df, frame)
 
     df = extract_overlaps(df)
-
+    #length of each identified orf
     df = extract_orf_length(df)
-
+    #total number of orfs per utr
     df = extract_orf_number(df)
-
+    #total number of augs per utr
     df = extract_aug_number(df)
 
     return df
@@ -270,6 +246,11 @@ def analyze(df):
 ######################################################################################################
 
 def barplot_mrl(data):
+    '''
+    input: dataframe containing utr sequences as well as corresponding measured
+    ans predicted MRL values
+    output: dataframe which can be plotted with sns.boxplot(..., hue='pred')
+    '''
 
     dfx = pd.DataFrame()
     dfx['MRL'] = data['rl']
@@ -277,7 +258,7 @@ def barplot_mrl(data):
     dfx['feature'] = np.nan
     dfx['pred'] = [False for row in dfx.index]
 
-    dfy =  pd.DataFrame()
+    dfy = pd.DataFrame()
     dfy['MRL'] = data['pred']
     dfy['utr'] = data['utr']
     dfy['feature'] = np.nan
@@ -303,16 +284,14 @@ def barplot_mrl(data):
     comp_data = pd.concat([dfx, dfy], axis = 0)
 
     return comp_data
-
-
-
-
-
 ######################################################################################################
 
 def r2(x,y):
-    r_value=stats.linregress(x,y)
+    slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
     return r_value**2
+
+
+######################################################################################################
 
 def corplot(df, obs_col='rl', pred_col='pred'):
 
